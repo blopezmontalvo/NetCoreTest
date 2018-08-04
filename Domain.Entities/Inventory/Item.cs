8451979fdb6edb3e034951974b3ce47537b19810
Brayan Lopez <blopezmontalvo@gmail.com>
2018-08-04 17:45:50 -0500

    mi primer commit

---
+++ Domain.Entities/Inventory/Item.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities.Inventory
+{
+    public class Item:NamedEntity
+    {
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public  int Quantity { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
